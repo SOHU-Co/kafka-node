082fc288422c008fd0aada2bfd84006aad85aed1
zhongyu <mockingror@gmail.com>
2013-08-25 03:10:08 +0000

    refact consumer

--- example/producer.js
+++ example/producer.js
@@ -2,9 +2,28 @@ var Producer = require('../lib/producer');
 
 var producer = new Producer();
 
+var letters = 'abcdefghijklmnopqrstuvwxyz',
+    upcaseLetters = letters.toUpperCase(),
+    numbers = '1234567890';
+
+var dictionary = letters + upcaseLetters + numbers;
+
+function createMsg() {
+    var digit = 2048 + Math.floor(Math.random()*2048);  
+    var charsNum = dictionary.length;
+    var n = Math.floor(digit/charsNum);
+    var n1 = digit%charsNum;
+
+    var ret = ''
+    for(var i=0;i<n;i++) {
+        ret += dictionary;
+    }
+    return ret +  dictionary.slice(n1);
+}
+for (var i=0; i<2000; i++) {
 producer.send([ 
-    {topic: 'sdsh-topic', message: 'hello world3'},
-    {topic: 't-topic', message: 'hello world3'} 
+    {topic: '10i-0', message: 'fdf'}
 ],function (data) {
-    console.log(data);
+    //console.log(data);
 });
+}
