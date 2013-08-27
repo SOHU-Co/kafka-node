788023aab4599870d2df8a2a7335f59c2ede35fd
Li Xin <jackxinli@sohu-inc.com>
2013-08-27 10:36:13 +0800

    fix a bug emit ready more than once.

--- example/producer.js
+++ example/producer.js
@@ -9,24 +9,25 @@ var letters = 'abcdefghijklmnopqrstuvwxyz',
 var dictionary = letters + upcaseLetters + numbers;
 
 function createMsg() {
-    var digit = 2048 + Math.floor(Math.random()*2048);  
+    var digit = 2048 + Math.floor(Math.random() * 2048);
     var charsNum = dictionary.length;
-    var n = Math.floor(digit/charsNum);
-    var n1 = digit%charsNum;
+    var n = Math.floor(digit / charsNum);
+    var n1 = digit % charsNum;
 
     var ret = ''
-    for(var i=0;i<n;i++) {
+    for (var i = 0; i < n; i++) {
         ret += dictionary;
     }
-    return ret +  dictionary.slice(n1);
+    return ret + dictionary.slice(n1);
 }
 producer.on('ready', function () {
-for (var i=0; i<20; i++) {
-producer.send([ 
-    {topic: 'cooltopic', message: 'hhhhhfdf'}
-],function (err,data) {
-    if (err) console.log(err);
-});
-}
+console.log('i am ready');
+   // for (var i = 0; i < 20; i++) {
+        producer.send([
+            {topic: 'cooltopic' + 7, message: 'topic-new' + 1 + 'coolmessage' }
+        ], function (err, data) {
+            if (err) console.log(err);
+        });
+   // }
 });
 
