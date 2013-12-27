bc452a99430c44d2c802c0a78dc6d0d3be52b4a5
zhongyu <mockingror@gmail.com>
2013-12-27 12:07:40 +0800

    change default fetchMaxBytes = 1024*1024

--- example/producer.js
+++ example/producer.js
@@ -8,35 +8,27 @@ var topic = argv.topic || 'topic1';
 
 var producer = new Producer(client);
 
-var letters = 'abcdefghijklmnopqrstuvwxyz',
-    upcaseLetters = letters.toUpperCase(),
-    numbers = '1234567890';
-
-var dictionary = letters + upcaseLetters + numbers;
-
-function createMsg() {
-    var digit = 2048 + Math.floor(Math.random() * 2048);
-    var charsNum = dictionary.length;
-    var n = Math.floor(digit / charsNum);
-    var n1 = digit % charsNum;
-
-    var ret = ''
-    for (var i = 0; i < n; i++) {
-        ret += dictionary;
-    }
-    return ret + dictionary.slice(n1);
-}
-
 var count = 1, rets = 0;
 producer.on('ready', function () {
    //setInterval(send, 1000); 
-   send();
+   send('hello');
 });
 
-function send() {
+function createMessage (n) {
+    var length = n * 1024;
+    var data = 'start';
+
+    for (var i=0; i<length; i++)
+        data = data + 'a';
+
+    return data + 'end';
+}
+
+function send(message) {
+    message = message || createMessage();
     for (var i = 0; i < count; i++) {
         producer.send([
-            {topic: topic, messages: ['777777777777777' + 2 + 'coolmessage'] }
+            {topic: topic, messages: [message] }
         ], function (err, data) {
             if (err) console.log(arguments);
             if (++rets === count) process.exit();
