4b3116c81b58bdad29507c1ea07b91f43b39e891
akram- <akram-@fake.fake>
2014-02-24 13:19:39 -0500

    Change way options are sent to producer class
    Correct bug where when we try to send 2 ProduceRequests with the same topic and partition, the app hangs
    
    Support producer sending messages to multiple partitions randomly or based on a key

--- test/test.producer.js
+++ test/test.producer.js
@@ -2,62 +2,94 @@
 
 var Producer = require('../lib/producer'),
     Client = require('../lib/client'),
+    Consumer = require('../lib/consumer'),
     config = require('./config');
 
 var client, producer;
 
 // Helper method
-function randomId () {
+function randomId() {
     return Math.floor(Math.random() * 10000)
 }
 
-before(function (done) {
-    client = new Client(config.zoo);
-    producer = new Producer(client);
-    producer.on('ready', function () {
-        producer.createTopics(['_exist_topic_3_test'], false, function (err, created) {
-           done(); 
+describe('Default Producer', function () {
+    before(function (done) {
+        client = new Client(config.zoo);
+        producer = new Producer(client, { partitionerType: 0 });
+        producer.on('ready', function () {
+            producer.createTopics(['_exist_topic_3_test'], false, function (err, created) {
+                done();
+            });
+        });
+    });
+
+    describe('Producer', function () {
+        describe('#send', function () {
+            it('should send message successfully', function (done) {
+                producer.send([{ topic: '_exist_topic_3_test', messages: 'hello kafka' }], function (err, message) {
+                    message.should.be.ok;
+                    done(err);
+                });
+            });
+
+            it('should support multi messages in one topic', function (done) {
+                producer.send([{ topic: '_exist_topic_3_test', messages: ['hello kafka', 'hello kafka'] }], function (err, message) {
+                    message.should.be.ok;
+                    done(err);
+                });
+            });
+        });
+
+        describe('#createTopics', function () {
+            it('should return All requests sent when async is true', function (done) {
+                producer.createTopics(['_exist_topic_' + randomId() + '_test'], true, function (err, data) {
+                    data.should.equal('All requests sent');
+                    done(err);
+                });
+            });
+
+            it('async should be true if not present', function (done) {
+                producer.createTopics(['_exist_topic_' + randomId() + '_test'], function (err, data) {
+                    data.should.equal('All requests sent');
+                    done(err);
+                });
+            });
+
+            it('should return All created when async is false', function (done) {
+                producer.createTopics(['_exist_topic_' + randomId() + '_test'], false, function (err, data) {
+                    data.should.equal('All created');
+                    done(err);
+                });
+            });
         });
     });
 });
 
-describe('Producer', function () {
-    describe('#send', function () {
-        it('should send message successfully', function (done) {
-            producer.send([{ topic: '_exist_topic_3_test', messages: 'hello kafka' }], function (err, message) {
-                message.should.be.ok;
-                done(err);
-            }); 
-        });
-
-        it('should support multi messages in one topic', function (done) {
-            producer.send([{ topic: '_exist_topic_3_test', messages: ['hello kafka', 'hello kafka'] }], function (err, message) {
-                message.should.be.ok;
-                done(err);
+describe('Partitioned Producer', function () {
+    before(function (done) {
+        client = new Client(config.zoo);
+        producer = new Producer(client, { partitionerType: 2 });
+        producer.on('ready', function () {
+            producer.createTopics(['_exist_topic_partitioned'], false, function (err, created) {
+                done();
             });
         });
     });
-    
-    describe('#createTopics', function () {
-        it('should return All requests sent when async is true', function (done) {
-            producer.createTopics(['_exist_topic_'+ randomId() +'_test'], true, function (err, data) {
-                data.should.equal('All requests sent'); 
-                done(err);
-            });
-        });
 
-        it('async should be true if not present', function (done) {
-            producer.createTopics(['_exist_topic_'+ randomId() +'_test'], function (err, data) {
-                data.should.equal('All requests sent'); 
-                done(err);
-            });
-        });
+    describe('Producer', function () {
+        describe('#send', function () {
+            it('should support partitioning messages according to their key', function (done) {
+                var msgs = [
+                    { key: 'key0', topic: '_exist_topic_partitioned', messages: ['hello kafka'] },
+                    { key: 'key1', topic: '_exist_topic_partitioned', messages: ['hello kafka'] },
+                    { key: 'key2', topic: '_exist_topic_partitioned', messages: ['hello kafka', 'hello kafka', 'hello kafka', 'hello kafka'] },
+                ]
 
-        it('should return All created when async is false', function (done) {
-            producer.createTopics(['_exist_topic_'+ randomId() +'_test'], false, function (err, data) {
-                data.should.equal('All created'); 
-                done(err);
+                producer.send(msgs, function (err, message) {
+                    message.should.be.ok;
+                    done(err);
+                });
             });
         });
-    }); 
+    });
 });
