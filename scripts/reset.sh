e18bd080bf8cf0736e88c2c3704afa9c134df2e3
Ted Ying <ted@remind101.com>
2014-05-27 13:03:24 -0700

    increase log level

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 rm -rf /usr/local/var/lib/kafka-logs*
 launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.zookeeper.plist
+sleep 1
 rm -rf /usr/local/var/run/zookeeper/data/*
 launchctl load ~/Library/LaunchAgents/homebrew.mxcl.zookeeper.plist
+#/usr/local/Cellar/kafka/0.8.1.1/bin/kafka-topics.sh --zookeeper localhost:2181 --topic test --create --partitions 1000 --replication-factor 2
