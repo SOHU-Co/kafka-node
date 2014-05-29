b91d2394e4e0744690567d13a25e52fd91c86f26
Ted Ying <ted@remind101.com>
2014-05-28 18:19:47 -0700

    print more errors

--- scripts/produce.sh
+++ scripts/produce.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # XXX this uses 1 partition
-seq 1000000 | /usr/local/Cellar/kafka/0.8.1.1/bin/kafka-console-producer.sh --broker-list localhost:9092,localhost:9093,localhost:9094 --topic test
+seq 1000000 | kafka-console-producer.sh --broker-list localhost:9092,localhost:9093,localhost:9094 --topic test
--- scripts/reset.sh
+++ scripts/reset.sh
@@ -4,4 +4,4 @@ launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.zookeeper.plist
 sleep 1
 rm -rf /usr/local/var/run/zookeeper/data/*
 launchctl load ~/Library/LaunchAgents/homebrew.mxcl.zookeeper.plist
-#/usr/local/Cellar/kafka/0.8.1.1/bin/kafka-topics.sh --zookeeper localhost:2181 --topic test --create --partitions 1000 --replication-factor 2
+#kafka-topics.sh --zookeeper localhost:2181 --topic test --create --partitions 1000 --replication-factor 2
