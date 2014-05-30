7cf9595c2e737cccfc73bcf71757829b5920ea22
Ted Ying <ted@remind101.com>
2014-05-29 17:42:32 -0700

    fix auto-commit

--- scripts/brokers.sh
+++ scripts/brokers.sh
@@ -6,4 +6,5 @@ do
     port=$[9092+i]
     /usr/local/Cellar/kafka/0.8.1.1/bin/kafka-server-start.sh <(sed -e s/broker\.id=0/broker.id=$i/g -e s/9092/$port/g -e s,/usr/local/var/lib/kafka-logs,\&.$i,g /usr/local/etc/kafka/server.properties) & kafka+=($!)
 done
+(sleep 3; kafka-topics.sh --zookeeper localhost:2181 --topic test --create --partitions 100 --replication-factor 2) & kafka+=($!)
 wait
--- scripts/reset.sh
+++ scripts/reset.sh
@@ -4,4 +4,5 @@ launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.zookeeper.plist
 sleep 1
 rm -rf /usr/local/var/run/zookeeper/data/*
 launchctl load ~/Library/LaunchAgents/homebrew.mxcl.zookeeper.plist
+ps ax | grep '[k]afka' | awk '{print $1}' | xargs kill -9
 #kafka-topics.sh --zookeeper localhost:2181 --topic test --create --partitions 1000 --replication-factor 2
