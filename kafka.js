768103aafd3bbe09d271ae6d7b9156bb90e49235
jezzalaycock <jeremylaycock@btinternet.com>
2014-06-20 12:53:36 +0100

    Adding highLevel Consumers and Producers

--- kafka.js
+++ kafka.js
@@ -1,3 +1,5 @@
+exports.HighLevelConsumer = require('./lib/highLevelConsumer');
+exports.HighLevelProducer = require('./lib/highLevelProducer');
 exports.Consumer = require('./lib/consumer');
 exports.Producer = require('./lib/producer');
 exports.Client = require('./lib/client')
