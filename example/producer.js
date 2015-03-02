9a69f0256c0cb6903a43cc995ea9fff5cb40df2d
haio <mockingror@gmail.com>
2015-03-02 14:21:16 +0800

    Export KeyedMessage
    Producer can send keyed message by create message with KeyedMessage(key,
    value)

--- example/producer.js
+++ example/producer.js
@@ -1,5 +1,6 @@
 var kafka = require('../kafka'),
     Producer = kafka.Producer,
+    KeyedMessage = kafka.KeyedMessage,
     Client = kafka.Client,
     client = new Client('localhost:2181');
 
@@ -11,17 +12,20 @@ var count = argv.count || 1, rets = 0;
 var producer = new Producer(client);
 
 producer.on('ready', function () {
-   send('hello');
+   send([
+     'hello',
+     new KeyedMessage('keyed', 'keyed message')
+   ]);
 });
 
 producer.on('error', function (err) {
     console.log('error', err)
 })
 
-function send(message) {
+function send(messages) {
     for (var i = 0; i < count; i++) {
         producer.send([
-            {topic: topic, messages: [message] , partition: p, attributes: a}
+            {topic: topic, messages: messages , partition: p, attributes: a}
         ], function (err, data) {
             if (err) console.log(arguments);
             if (++rets === count) process.exit();
