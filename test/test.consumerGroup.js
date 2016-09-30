68df3970905198a77ddb35f61e2b329fd148784a
Xiaoxin Lu <javascript@yahoo.com>
2016-09-30 10:16:09 -0400

    only one ConsumerGroup connect should be scheduled at a time

--- test/test.consumerGroup.js
+++ test/test.consumerGroup.js
@@ -1,13 +1,66 @@
 'use strict';
 
+var sinon = require('sinon');
 var should = require('should');
 var ConsumerGroup = require('../lib/ConsumerGroup');
 var host = process.env['KAFKA_TEST_HOST'] || '';
-var _ = require('lodash');
-var GroupCoordinatorNotAvailable = require('../lib/errors/GroupCoordinatorNotAvailableError');
-var GroupLoadInProgress = require('../lib/errors/GroupLoadInProgressError');
 
 
 describe('ConsumerGroup', function () {
+  describe('#scheduleReconnect', function () {
+    var consumerGroup, sandbox;
 
+    beforeEach(function () {
+      consumerGroup = new ConsumerGroup({
+        host: host,
+        connnectOnReady: false
+      }, ['TestTopic']);
+
+      sandbox = sinon.sandbox.create();
+      sandbox.stub(consumerGroup, 'connect');
+    });
+
+    afterEach(function () {
+      sandbox.restore();
+    })
+
+    it('should throw an exception if passed in a empty timeout', function () {
+      should.throws(function () {
+        consumerGroup.scheduleReconnect(0);
+      });
+
+      should.throws(function () {
+        consumerGroup.scheduleReconnect(null);
+      });
+
+      should.throws(function () {
+        consumerGroup.scheduleReconnect(undefined);
+      });
+    });
+
+    it('should call connect method when scheduled timer is up', function () {
+      sandbox.useFakeTimers();
+      should(consumerGroup.reconnectTimer).be.undefined;
+      consumerGroup.scheduleReconnect(1000);
+      should(consumerGroup.reconnectTimer).exist;
+      sandbox.clock.tick(1000);
+      should(consumerGroup.reconnectTimer).be.null;
+      sinon.assert.calledOnce(consumerGroup.connect);
+    });
+
+    it('should only allow one connect to be scheduled at a time', function () {
+      sandbox.useFakeTimers();
+      consumerGroup.scheduleReconnect(2000);
+      sandbox.clock.tick(1000);
+      consumerGroup.scheduleReconnect(2000);
+      consumerGroup.scheduleReconnect(2000);
+      consumerGroup.scheduleReconnect(2000);
+      sandbox.clock.tick(2000);
+      should(consumerGroup.reconnectTimer).be.null;
+      sinon.assert.calledOnce(consumerGroup.connect);
+
+      sandbox.clock.tick(10000);
+      sinon.assert.calledOnce(consumerGroup.connect);
+    });
+  });
 });
