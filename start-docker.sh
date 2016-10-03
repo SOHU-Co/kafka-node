e0db2be257a4f0411687ae8b16cce43b64f8dc99
Xiaoxin Lu <javascript@yahoo.com>
2016-10-03 09:52:02 -0400

    Remove support for docker-machine (#474)

--- start-docker.sh
+++ start-docker.sh
@@ -11,19 +11,6 @@ if [ -z "$TRAVIS" ]; then
     elif docker info | grep dlite > /dev/null; then
         echo "Looks like docker based on dlite is running"
         DOCKER_VM_IP=`dlite ip`
-    else
-        DOCKER_VM_IP=`docker-machine ip dev`
-
-        if [ "$DOCKER_VM_IP" != "" ]; then
-            echo "Docker VM is running."
-            echo "Docker VM IP Address: $DOCKER_VM_IP"
-        else
-            echo "Docker VM is not running."
-            docker-machine start dev
-            DOCKER_VM_IP=`docker-machine ip dev`
-        fi
-
-        eval "$(docker-machine env dev)"
     fi
 
     export KAFKA_ADVERTISED_HOST_NAME=$DOCKER_VM_IP
