2e9252bf934d4c40cc48cb6ed93faf5f28266718
Xiaoxin Lu <javascript@yahoo.com>
2017-05-22 17:29:07 -0400

    Added method to refresh the broker metadata

--- test/test.kafkaClient.js
+++ test/test.kafkaClient.js
@@ -4,6 +4,7 @@ const kafka = require('..');
 const Client = kafka.KafkaClient;
 const sinon = require('sinon');
 const TimeoutError = require('../lib/errors/TimeoutError');
+const BrokerWrapper = require('../lib/wrapper/BrokerWrapper');
 const FakeSocket = require('./mocks/mockSocket');
 const should = require('should');
 const _ = require('lodash');
@@ -153,7 +154,7 @@ describe('Kafka Client', function () {
       clock.tick(100);
     });
 
-    it('should set different brokerMetadata field on client no emit', function (done) {
+    it('should set different brokerMetadata field on client emit', function (done) {
       const client = new Client({
         autoConnect: false,
         kafkaHost: 'Kafka-1.us-east-1.myapp.com:9093'
@@ -216,6 +217,111 @@ describe('Kafka Client', function () {
         client.brokerMetadata.should.not.be.empty;
         done();
       });
+    });
+  });
+
+  describe('#refreshBrokerMetadata', function () {
+    let sandbox, client;
+
+    beforeEach(function () {
+      sandbox = sinon.sandbox.create();
+      client = new Client({
+        connectRetryOptions: {
+          retries: 0
+        },
+        autoConnect: false,
+        kafkaHost: 'localhost:9093',
+        sslOptions: {
+          rejectUnauthorized: false
+        }
+      });
+    });
+
+    afterEach(function () {
+      sandbox.restore();
+    });
+
+    it('should refresh broker metadata using available broker', function (done) {
+      const fakeBroker = new BrokerWrapper(new FakeSocket());
+      const fakeDeadBroker = new BrokerWrapper(new FakeSocket());
+
+      const metadata = [
+        {
+          '1': { nodeId: 1, host: 'Kafka-1.us-east-1.myapp.com', port: 9093 },
+          '2': { nodeId: 2, host: 'Kafka-2.us-east-1.myapp.com', port: 9093 },
+          '3': { nodeId: 3, host: 'Kafka-3.us-east-1.myapp.com', port: 9093 }
+        },
+        { metadata: {} }
+      ];
+
+      client.brokerMetadata = _.clone(metadata[0]);
+
+      const newBrokerKey = `${metadata[0][2].host}:${metadata[0][2].port}`;
+      const deadBrokerKey = `${metadata[0][1].host}:${metadata[0][1].port}`;
+
+      client.brokers[deadBrokerKey] = fakeDeadBroker;
+      client.brokers[newBrokerKey] = fakeBroker;
+
+      delete metadata[0]['1'];
+
+      Object.assign(fakeBroker.socket, metadata['2']);
+
+      sandbox.stub(client, 'getAvailableBroker').yields(null, fakeBroker);
+      sandbox.stub(client, 'loadMetadataFrom').yields(null, metadata);
+      sandbox.spy(client, 'updateMetadatas');
+      sandbox.spy(client, 'refreshBrokers');
+
+      should(client.refreshingMetadata).be.empty;
+
+      client.refreshBrokerMetadata(function (error) {
+        sinon.assert.calledOnce(client.getAvailableBroker);
+        sinon.assert.calledWith(client.loadMetadataFrom, fakeBroker, sinon.match.func);
+        sinon.assert.calledWith(client.updateMetadatas, metadata);
+        sinon.assert.calledOnce(client.refreshBrokers);
+        sinon.assert.callOrder(
+          client.getAvailableBroker,
+          client.loadMetadataFrom,
+          client.updateMetadatas,
+          client.refreshBrokers
+        );
+
+        client.brokers.should.have.property(newBrokerKey).and.be.exactly(fakeBroker);
+        client.brokers.should.not.have.property(deadBrokerKey);
+
+        done(error);
+      });
+    });
+
+    it('should emit an error', function (done) {
+      const expectedError = new Error('Unable to find available brokers to try');
+      sandbox.stub(client, 'getAvailableBroker').yields(expectedError);
+      sandbox.stub(client, 'loadMetadataFrom');
+      sandbox.stub(client, 'updateMetadatas');
+      sandbox.stub(client, 'refreshBrokers');
+
+      client.on('error', function (error) {
+        error.should.be.an.instanceOf(Error);
+        error.nested.should.be.eql(expectedError);
+        done();
+      });
+
+      client.refreshBrokerMetadata();
+    });
+
+    it('should not perform refreshBrokerMetadata if one is in progress', function () {
+      sandbox.stub(client, 'getAvailableBroker');
+      sandbox.stub(client, 'loadMetadataFrom');
+      sandbox.stub(client, 'updateMetadatas');
+      sandbox.stub(client, 'refreshBrokers');
+      client.refreshingMetadata = true;
+
+      client.refreshBrokerMetadata();
+
+      client.refreshingMetadata.should.be.true;
+      sinon.assert.notCalled(client.getAvailableBroker);
+      sinon.assert.notCalled(client.loadMetadataFrom);
+      sinon.assert.notCalled(client.updateMetadatas);
+      sinon.assert.notCalled(client.refreshBrokers);
     });
   });
 
