cdd6ab3cc125602bde5c2cfd24f076a6e923418e
Xiaoxin Lu <javascript@yahoo.com>
2017-07-06 10:26:04 -0400

    Allow any none empty value to be saved as message key

--- test/test.baseProducer.js
+++ test/test.baseProducer.js
@@ -1,10 +1,175 @@
 'use strict';
 
 const BaseProducer = require('../lib/baseProducer');
+const ConsumerGroup = require('../lib/consumerGroup');
+const KafkaClient = require('../lib/kafkaClient');
 const Client = require('./mocks/mockClient');
+const uuid = require('uuid');
 const sinon = require('sinon');
+const async = require('async');
+const should = require('should');
 
 describe('BaseProducer', function () {
+  const KAFKA_HOST = 'localhost:9092';
+  describe('key attribute', function () {
+    let consumerGroup, topic, producer;
+    beforeEach(function (done) {
+      topic = uuid.v4();
+
+      const createTopic = require('../docker/createTopic');
+
+      async.series(
+        [
+          function (callback) {
+            createTopic(topic, 1, 1)
+              .then(function () {
+                callback(null);
+              })
+              .catch(error => callback(error));
+          },
+          function (callback) {
+            const client = new KafkaClient({
+              kafkaHost: KAFKA_HOST
+            });
+
+            producer = new BaseProducer(client, {}, BaseProducer.PARTITIONER_TYPES.default);
+            producer.once('ready', function () {
+              callback(null);
+            });
+          },
+          function (callback) {
+            consumerGroup = new ConsumerGroup(
+              {
+                kafkaHost: KAFKA_HOST,
+                groupId: uuid.v4()
+              },
+              topic
+            );
+            consumerGroup.once('connect', function () {
+              callback(null);
+            });
+          }
+        ],
+        done
+      );
+    });
+
+    afterEach(function (done) {
+      producer.close();
+      consumerGroup.close(done);
+    });
+
+    it('verify key string value makes it into the message', function (done) {
+      producer.send(
+        [
+          {
+            topic: topic,
+            messages: 'this is my message',
+            key: 'myKeyIsHere'
+          }
+        ],
+        function (error) {
+          if (error) {
+            done(error);
+          }
+        }
+      );
+
+      consumerGroup.on('message', function (message) {
+        message.key.toString().should.be.exactly('myKeyIsHere');
+        done();
+      });
+    });
+
+    it('verify empty key string value makes it into the message', function (done) {
+      producer.send(
+        [
+          {
+            topic: topic,
+            messages: 'this is my message',
+            key: ''
+          }
+        ],
+        function (error) {
+          if (error) {
+            done(error);
+          }
+        }
+      );
+
+      consumerGroup.on('message', function (message) {
+        message.key.toString().should.be.exactly('');
+        done();
+      });
+    });
+
+    it('verify key value of 0 makes it into the message', function (done) {
+      producer.send(
+        [
+          {
+            topic: topic,
+            messages: 'this is my message',
+            key: 0
+          }
+        ],
+        function (error) {
+          if (error) {
+            done(error);
+          }
+        }
+      );
+
+      consumerGroup.on('message', function (message) {
+        message.key.toString().should.be.exactly('0');
+        done();
+      });
+    });
+
+    it('verify key value of null makes it into the message as null', function (done) {
+      producer.send(
+        [
+          {
+            topic: topic,
+            messages: 'this is my message',
+            key: null
+          }
+        ],
+        function (error) {
+          if (error) {
+            done(error);
+          }
+        }
+      );
+
+      consumerGroup.on('message', function (message) {
+        should(message.key).be.null;
+        done();
+      });
+    });
+
+    it('verify key value of undefined makes it into the message as null', function (done) {
+      producer.send(
+        [
+          {
+            topic: topic,
+            messages: 'this is my message',
+            key: undefined
+          }
+        ],
+        function (error) {
+          if (error) {
+            done(error);
+          }
+        }
+      );
+
+      consumerGroup.on('message', function (message) {
+        should(message.key).be.null;
+        done();
+      });
+    });
+  });
+
   describe('On Brokers Changed', function () {
     it('should emit error when refreshMetadata fails', function (done) {
       const fakeClient = new Client();
