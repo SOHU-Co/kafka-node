9d2ede228cfa7d06693afa37d6a62edc7f219393
Xiaoxin Lu <javascript@yahoo.com>
2018-01-05 16:16:05 -0500

    formatting

--- example/consumer.js
+++ example/consumer.js
@@ -8,10 +8,7 @@ var argv = require('optimist').argv;
 var topic = argv.topic || 'topic1';
 
 var client = new Client('localhost:2181');
-var topics = [
-    {topic: topic, partition: 1},
-    {topic: topic, partition: 0}
-];
+var topics = [{ topic: topic, partition: 1 }, { topic: topic, partition: 0 }];
 var options = { autoCommit: false, fetchMaxWaitMs: 1000, fetchMaxBytes: 1024 * 1024 };
 
 var consumer = new Consumer(client, topics, options);
