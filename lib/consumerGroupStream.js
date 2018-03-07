SOHU-Co/kafka-node

commit abc160986765ebf94b121d56cadaa4a3d426559a

Fix offset committed in ConsumerGroupStream being off by one closes #814 (#850)

lib/consumerGroupStream.js
@@ -19,7 +19,7 @@ function convertToCommitPayload (messages) {
         ret.push({
           topic: topic,
           partition: partition,
-          offset: offset,
+          offset: offset + 1,
           metadata: 'm'
         });
       }

test/test.consumerGroupStream.js
@@ -9,8 +9,8 @@ const sinon = require('sinon');
 
 const ConsumerGroupStream = require('../lib/consumerGroupStream');
 
-function createConsumerGroupStream (topic) {
-  const consumerOptions = {
+function createConsumerGroupStream (topic, overrides) {
+  const consumerOptions = _.defaultsDeep(overrides || {}, {
     kafkaHost: '127.0.0.1:9092',
     groupId: 'ExampleTestGroup',
     sessionTimeout: 15000,
@@ -19,16 +19,70 @@ function createConsumerGroupStream (topic) {
     id: 'consumer1',
     autoCommit: false,
     fromOffset: 'earliest'
-  };
+  });
   return new ConsumerGroupStream(consumerOptions, topic);
 }
 
 describe('ConsumerGroupStream', function () {
-  let topic;
+  let topic, groupId;
 
-  before(function () {
+  beforeEach(function () {
     topic = uuid.v4();
+    groupId = uuid.v4();
     return createTopic(topic, 1, 1);
+  });
+
+  describe('Auto Commit', function () {
+    it('should resume at the next offset after new stream starts', function (done) {
+      let messages;
+      let consumerGroupStream;
+      let lastReadOffset;
+      async.series(
+        [
+          function (callback) {
+            messages = _.times(3, uuid.v4);
+            sendMessage(messages, topic, callback);
+          },
+
+          function (callback) {
+            const messagesToRead = _.clone(messages);
+            consumerGroupStream = createConsumerGroupStream(topic, { autoCommit: true, groupId: groupId });
+            consumerGroupStream.on('data', function (message) {
+              _.pull(messagesToRead, message.value, message.offset);
+              if (messagesToRead.length === 0) {
+                lastReadOffset = message.offset;
+                callback(null);
+              }
+            });
+          },
+
+          function (callback) {
+            setImmediate(function () {
+              consumerGroupStream.close(callback);
+              consumerGroupStream = null;
+            });
+          },
+
+          function (callback) {
+            setTimeout(callback, 100);
+          },
+
+          function (callback) {
+            sendMessage([uuid.v4()], topic, callback);
+          },
+
+          function (callback) {
+            consumerGroupStream = createConsumerGroupStream(topic, { autoCommit: true, groupId: groupId });
+            consumerGroupStream.on('readable', function () {
+              const message = consumerGroupStream.read();
+              message.offset.should.be.equal(lastReadOffset + 1);
+              consumerGroupStream.close(callback);
+            });
+          }
+        ],
+        done
+      );
+    });
   });
 
   describe('#close', function () {
@@ -55,7 +109,7 @@ describe('ConsumerGroupStream', function () {
           },
           function (callback) {
             const messagesToRead = _.clone(messages);
-            consumerGroupStream = createConsumerGroupStream(topic);
+            consumerGroupStream = createConsumerGroupStream(topic, { groupId: groupId });
             consumerGroupStream.on('data', function (message) {
               _.pull(messagesToRead, message.value);
               if (messagesToRead.length === 0) {
@@ -68,7 +122,7 @@ describe('ConsumerGroupStream', function () {
           },
           function (callback) {
             const messagesToRead = _.clone(messages);
-            consumerGroupStream = createConsumerGroupStream(topic);
+            consumerGroupStream = createConsumerGroupStream(topic, { groupId: groupId });
             consumerGroupStream.on('data', function (message) {
               _.pull(messagesToRead, message.value);
               if (messagesToRead.length === 0) {

commit b3a76a316258ae7781173ef6b173441f07c0cf63

Fix Stale Commit Queue for ConsumerGroupStream
The commit at https://github.com/SOHU-Co/kafka-node/commit/abc160986765ebf94b121d56cadaa4a3d426559a introduced a bug
where the offset was incremented at the wrong location.

commitQueued then expected the offset committed to match what is currently in the queue, and that commit broke this.

This results in commit records never being correctly removed from the commitQueue, leading to
weird behavior when committing and messages being consumed twice when rebalances occur relating
to committing stale offsets.

lib/consumerGroupStream.js
@@ -19,7 +19,7 @@ function convertToCommitPayload (messages) {
         ret.push({
           topic: topic,
           partition: partition,
-          offset: offset + 1,
+          offset: offset,
           metadata: 'm'
         });
       }
@@ -73,7 +73,7 @@ class ConsumerGroupStream extends Readable {
 
   commit (message, force, callback) {
     if (message != null && message.offset !== -1) {
-      _.set(this.commitQueue, [message.topic, message.partition], message.offset);
+      _.set(this.commitQueue, [message.topic, message.partition], message.offset + 1);
     }
 
     if (this.committing && !force) {