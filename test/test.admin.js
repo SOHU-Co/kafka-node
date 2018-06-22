9f6106b92a702ba2c98e4589ca1514f506b1d121
ahmedlafta <aalafta@gmail.com>
2018-06-22 14:40:04 -0500

    fix describeGroups error handling

--- test/test.admin.js
+++ test/test.admin.js
@@ -4,7 +4,7 @@ const Admin = require('../lib/admin');
 const ConsumerGroup = require('../lib/consumerGroup');
 const uuid = require('uuid');
 
-describe('Admin', function () {
+describe.only('Admin', function () {
   describe('#listGroups', function () {
     const createTopic = require('../docker/createTopic');
     let admin, consumer;
@@ -36,6 +36,54 @@ describe('Admin', function () {
       admin.listGroups(function (error, res) {
         res.should.have.keys(groupId);
         res[groupId].should.eql('consumer');
+        done(error);
+      });
+    });
+  });
+
+  describe('#describeGroups', function () {
+    const createTopic = require('../docker/createTopic');
+    let admin, consumer;
+    const topic = uuid.v4();
+    const groupId = 'test-group-id';
+
+    before(function (done) {
+      if (process.env.KAFKA_VERSION === '0.8') {
+        this.skip();
+      }
+
+      createTopic(topic, 1, 1).then(function () {
+        consumer = new ConsumerGroup({
+          kafkaHost: 'localhost:9092',
+          groupId: groupId
+        }, topic);
+        consumer.once('connect', function () {
+          done();
+        });
+      });
+    });
+
+    after(function (done) {
+      consumer.close(done);
+    });
+
+    it('should describe a list of consumer groups', function (done) {
+      admin = new Admin(consumer.client);
+      admin.describeGroups([groupId], function (error, res) {
+        res.should.have.keys(groupId);
+        res[groupId].should.have.property('members').with.lengthOf(1);
+        res[groupId].should.have.property('state', 'Stable');
+        done(error);
+      });
+    });
+
+    it('should return empty members if consumer group doesnt exist', function (done) {
+      admin = new Admin(consumer.client);
+      const nonExistentGroup = 'non-existent-group';
+      admin.describeGroups([nonExistentGroup], function (error, res) {
+        res.should.have.keys(nonExistentGroup);
+        res[nonExistentGroup].should.have.property('members').with.lengthOf(0);
+        res[nonExistentGroup].should.have.property('state', 'Dead');
         done(error);
       });
     });
