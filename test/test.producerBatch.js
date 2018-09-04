4beae1aeca3a8ebbb77b76eccdfe0247a7845bfc
Xiaoxin Lu <javascript@yahoo.com>
2018-09-04 14:31:40 -0400

    Streams became more async in node 10

--- test/test.producerBatch.js
+++ test/test.producerBatch.js
@@ -20,40 +20,47 @@ var broker = null;
 
 describe('No Ack Producer', function () {
   before(function (done) {
-    async.series({
-      setupClient: function (callback) {
-        client = new Client(host);
-        batchClient = new Client(host, null, null, { noAckBatchSize: BATCH_SIZE, noAckBatchAge: BATCH_AGE });
-        producer = new Producer(client);
-        batchProducer = new Producer(batchClient);
-        producer.on('ready', function () {
-          producer.createTopics([EXISTS_TOPIC_4], true, function (err) {
+    async.series(
+      {
+        setupClient: function (callback) {
+          client = new Client(host);
+          batchClient = new Client(host, null, null, { noAckBatchSize: BATCH_SIZE, noAckBatchAge: BATCH_AGE });
+          producer = new Producer(client);
+          batchProducer = new Producer(batchClient);
+          producer.on('ready', function () {
+            producer.createTopics([EXISTS_TOPIC_4], true, function (err) {
+              if (err) return callback(err);
+              callback();
+            });
+            broker = Object.keys(client.brokers)[0];
+          });
+        },
+        producerSend: function (callback) {
+          producer.send([{ topic: EXISTS_TOPIC_4, messages: '_initial 1' }], function (err, message) {
             if (err) return callback(err);
-            callback();
-          });
-          broker = Object.keys(client.brokers)[0];
-        });
-      },
-      producerSend: function (callback) {
-        producer.send([{ topic: EXISTS_TOPIC_4, messages: '_initial 1' }], function (err, message) {
-          if (err) return callback(err);
-          message.should.be.ok;
-          message[EXISTS_TOPIC_4].should.have.property('0', 0);
-          batchProducer.send([{ topic: EXISTS_TOPIC_4, messages: '_initial 2' }], function (err, message) {
             message.should.be.ok;
-            message[EXISTS_TOPIC_4].should.have.property('0', 1);
-            callback(err);
+            message[EXISTS_TOPIC_4].should.have.property('0', 0);
+            batchProducer.send([{ topic: EXISTS_TOPIC_4, messages: '_initial 2' }], function (err, message) {
+              message.should.be.ok;
+              message[EXISTS_TOPIC_4].should.have.property('0', 1);
+              callback(err);
+            });
           });
-        });
-      // Ensure that first message gets the `0`
-      }
-    }, done);
+          // Ensure that first message gets the `0`
+        }
+      },
+      done
+    );
   });
 
   after(function (done) {
-    async.each([producer, batchProducer], function (producer, callback) {
-      producer.close(callback);
-    }, done);
+    async.each(
+      [producer, batchProducer],
+      function (producer, callback) {
+        producer.close(callback);
+      },
+      done
+    );
   });
 
   describe('with no batch client', function () {
@@ -72,15 +79,23 @@ describe('No Ack Producer', function () {
 
     it('should send message directly', function (done) {
       var self = this;
-      noAckProducer.send([{
-        topic: EXISTS_TOPIC_4, messages: 'hello kafka no batch'
-      }], function (err, message) {
-        if (err) return done(err);
-        message.result.should.equal('no ack');
-        self.sendSpy.args.length.should.be.equal(1);
-        self.sendSpy.args[0].toString().should.containEql('hello kafka no batch');
-        done();
-      });
+      noAckProducer.send(
+        [
+          {
+            topic: EXISTS_TOPIC_4,
+            messages: 'hello kafka no batch'
+          }
+        ],
+        function (err, message) {
+          if (err) return done(err);
+          setImmediate(function () {
+            message.result.should.equal('no ack');
+            self.sendSpy.args.length.should.be.equal(1);
+            self.sendSpy.args[0].toString().should.containEql('hello kafka no batch');
+            done();
+          });
+        }
+      );
     });
   });
 
@@ -92,7 +107,7 @@ describe('No Ack Producer', function () {
 
     beforeEach(function () {
       this.sendSpy = sinon.spy(batchClient.brokers[broker].socket, 'write');
-      this.clock = sinon.useFakeTimers();
+      this.clock = sinon.useFakeTimers(0, 'setTimeout');
     });
 
     afterEach(function () {
@@ -102,19 +117,29 @@ describe('No Ack Producer', function () {
 
     it('should wait to send message 500 ms', function (done) {
       var self = this;
-      noAckProducer.send([{
-        topic: EXISTS_TOPIC_4, messages: 'hello kafka with batch'
-      }], function (err, message) {
-        if (err) return done(err);
-        message.result.should.equal('no ack');
-        self.sendSpy.args.length.should.be.equal(0);
-        self.clock.tick(BATCH_AGE - 5);
-        self.sendSpy.args.length.should.be.equal(0);
-        self.clock.tick(10);
-        self.sendSpy.args.length.should.be.equal(1);
-        self.sendSpy.args[0].toString().should.containEql('hello kafka with batch');
-        done();
-      });
+      noAckProducer.send(
+        [
+          {
+            topic: EXISTS_TOPIC_4,
+            messages: 'hello kafka with batch'
+          }
+        ],
+        function (err, message) {
+          if (err) return done(err);
+          setImmediate(function () {
+            message.result.should.equal('no ack');
+            self.sendSpy.args.length.should.be.equal(0);
+            self.clock.tick(BATCH_AGE - 5);
+            self.sendSpy.args.length.should.be.equal(0);
+            self.clock.tick(10);
+            setImmediate(function () {
+              self.sendSpy.args.length.should.be.equal(1);
+              self.sendSpy.args[0].toString().should.containEql('hello kafka with batch');
+              done();
+            });
+          });
+        }
+      );
     });
 
     it('should send message once the batch max size is reached', function (done) {
@@ -122,23 +147,37 @@ describe('No Ack Producer', function () {
       var foo = '';
       for (var i = 0; i < BATCH_SIZE; i++) foo += 'X';
       foo += 'end of message';
-      noAckProducer.send([{
-        topic: EXISTS_TOPIC_4, messages: 'hello kafka with batch'
-      }], function (err, message) {
-        if (err) return done(err);
-        message.result.should.equal('no ack');
-        self.sendSpy.args.length.should.be.equal(0);
-        noAckProducer.send([{
-          topic: EXISTS_TOPIC_4, messages: foo
-        }], function (err, message) {
+      noAckProducer.send(
+        [
+          {
+            topic: EXISTS_TOPIC_4,
+            messages: 'hello kafka with batch'
+          }
+        ],
+        function (err, message) {
           if (err) return done(err);
           message.result.should.equal('no ack');
-          self.sendSpy.args.length.should.be.equal(1);
-          self.sendSpy.args[0].toString().should.containEql('hello kafka with batch');
-          self.sendSpy.args[0].toString().should.containEql('end of message');
-          done();
-        });
-      });
+          self.sendSpy.args.length.should.be.equal(0);
+          noAckProducer.send(
+            [
+              {
+                topic: EXISTS_TOPIC_4,
+                messages: foo
+              }
+            ],
+            function (err, message) {
+              if (err) return done(err);
+              setImmediate(function () {
+                message.result.should.equal('no ack');
+                self.sendSpy.args.length.should.be.equal(1);
+                self.sendSpy.args[0].toString().should.containEql('hello kafka with batch');
+                self.sendSpy.args[0].toString().should.containEql('end of message');
+                done();
+              });
+            }
+          );
+        }
+      );
     });
   });
 });
