76f6939771349b31053aa811173967a987d7c0e1
Xiaoxin Lu <javascript@yahoo.com>
2018-09-07 14:18:44 -0400

    Add test for PR #1066 (#1070)

--- test/test.consumerGroupStream.js
+++ test/test.consumerGroupStream.js
@@ -6,6 +6,8 @@ const sendMessage = require('./helpers/sendMessage');
 const async = require('async');
 const _ = require('lodash');
 const sinon = require('sinon');
+const proxyquire = require('proxyquire');
+const EventEmitter = require('events');
 
 const ConsumerGroupStream = require('../lib/consumerGroupStream');
 
@@ -24,119 +26,202 @@ function createConsumerGroupStream (topic, overrides) {
 }
 
 describe('ConsumerGroupStream', function () {
-  let topic, groupId;
+  let topic, groupId, sandbox;
 
-  beforeEach(function () {
-    topic = uuid.v4();
-    groupId = uuid.v4();
-    return createTopic(topic, 1, 1);
-  });
+  context('autoCommit', function () {
+    let consumerGroupStream, ConsumerGroupStream, fakeConsumerGroup;
 
-  describe('Auto Commit', function () {
-    it('should resume at the next offset after new stream starts', function (done) {
-      let messages;
-      let consumerGroupStream;
-      let lastReadOffset;
-      async.series(
-        [
-          function (callback) {
-            messages = _.times(3, uuid.v4);
-            sendMessage(messages, topic, callback);
-          },
-
-          function (callback) {
-            callback = _.once(callback);
-            const messagesToRead = _.clone(messages);
-            consumerGroupStream = createConsumerGroupStream(topic, { autoCommit: true, groupId: groupId });
-            consumerGroupStream.on('data', function (message) {
-              _.pull(messagesToRead, message.value, message.offset);
-              if (messagesToRead.length === 0) {
-                lastReadOffset = message.offset;
-                callback(null);
-              }
-            });
-          },
-
-          function (callback) {
-            setImmediate(function () {
-              consumerGroupStream.close(callback);
-              consumerGroupStream = null;
-            });
-          },
-
-          function (callback) {
-            setTimeout(callback, 100);
-          },
-
-          function (callback) {
-            sendMessage([uuid.v4()], topic, callback);
-          },
-
-          function (callback) {
-            consumerGroupStream = createConsumerGroupStream(topic, { autoCommit: true, groupId: groupId });
-            consumerGroupStream.on('readable', function () {
-              const message = consumerGroupStream.read();
-              message.offset.should.be.equal(lastReadOffset + 1);
-              consumerGroupStream.close(callback);
-            });
-          }
-        ],
-        done
-      );
-    });
-  });
-
-  describe('#close', function () {
-    it('should not call consumerGroup with force option', function (done) {
-      const consumerGroupStream = createConsumerGroupStream(topic);
-
-      const closeSpy = sinon.spy(consumerGroupStream.consumerGroup, 'close');
-
-      consumerGroupStream.close(function () {
-        sinon.assert.calledOnce(closeSpy);
-        sinon.assert.calledWithExactly(closeSpy, false, sinon.match.func);
-        done();
+    before(function () {
+      sandbox = sinon.sandbox.create({
+        useFakeTimers: true
+      });
+      fakeConsumerGroup = new EventEmitter();
+      _.set(fakeConsumerGroup, 'options.autoCommitIntervalMs', 500);
+      ConsumerGroupStream = proxyquire('../lib/consumerGroupStream', {
+        './consumerGroup': function () {
+          return fakeConsumerGroup;
+        }
       });
     });
 
-    it('autoCommit false should close the consumer without committing offsets', function (done) {
-      const messages = _.times(3, uuid.v4);
-      let consumerGroupStream;
+    beforeEach(function () {
+      consumerGroupStream = new ConsumerGroupStream({});
+      sandbox.reset();
+    });
 
+    after(function () {
+      sandbox.restore();
+    });
+
+    it('should continue to work work after force true commit', function (done) {
+      fakeConsumerGroup.sendOffsetCommitRequest = sinon.stub().yieldsAsync(null);
       async.series(
         [
           function (callback) {
-            sendMessage(messages, topic, callback);
+            consumerGroupStream.commit(
+              {
+                topic: 'MyTestTopic',
+                partition: 0,
+                offset: 1
+              },
+              true,
+              callback
+            );
           },
           function (callback) {
-            const messagesToRead = _.clone(messages);
-            consumerGroupStream = createConsumerGroupStream(topic, { groupId: groupId });
-            consumerGroupStream.on('data', function (message) {
-              _.pull(messagesToRead, message.value);
-              if (messagesToRead.length === 0) {
-                callback(null);
-              }
-            });
-          },
-          function (callback) {
-            consumerGroupStream.close(callback);
-          },
-          function (callback) {
-            const messagesToRead = _.clone(messages);
-            consumerGroupStream = createConsumerGroupStream(topic, { groupId: groupId });
-            consumerGroupStream.on('data', function (message) {
-              _.pull(messagesToRead, message.value);
-              if (messagesToRead.length === 0) {
-                callback(null);
-              }
-            });
-          },
-          function (callback) {
-            consumerGroupStream.close(callback);
+            consumerGroupStream.commit(
+              {
+                topic: 'MyTestTopic',
+                partition: 0,
+                offset: 5
+              },
+              false,
+              callback
+            );
           }
         ],
-        done
+        function (error) {
+          sinon.assert.calledTwice(fakeConsumerGroup.sendOffsetCommitRequest);
+          sinon.assert.calledWith(fakeConsumerGroup.sendOffsetCommitRequest, [
+            {
+              metadata: 'm',
+              offset: 2,
+              partition: 0,
+              topic: 'MyTestTopic'
+            }
+          ]);
+
+          sinon.assert.calledWith(fakeConsumerGroup.sendOffsetCommitRequest, [
+            {
+              metadata: 'm',
+              offset: 6,
+              partition: 0,
+              topic: 'MyTestTopic'
+            }
+          ]);
+
+          consumerGroupStream.committing.should.be.true;
+          sandbox.clock.tick(500);
+          consumerGroupStream.committing.should.be.false;
+          done(error);
+        }
       );
+    });
+  });
+
+  context('with topic', function () {
+    beforeEach(function () {
+      topic = uuid.v4();
+      groupId = uuid.v4();
+      return createTopic(topic, 1, 1);
+    });
+
+    describe('Auto Commit', function () {
+      it('should resume at the next offset after new stream starts', function (done) {
+        let messages;
+        let consumerGroupStream;
+        let lastReadOffset;
+        async.series(
+          [
+            function (callback) {
+              messages = _.times(3, uuid.v4);
+              sendMessage(messages, topic, callback);
+            },
+
+            function (callback) {
+              callback = _.once(callback);
+              const messagesToRead = _.clone(messages);
+              consumerGroupStream = createConsumerGroupStream(topic, { autoCommit: true, groupId: groupId });
+              consumerGroupStream.on('data', function (message) {
+                _.pull(messagesToRead, message.value, message.offset);
+                if (messagesToRead.length === 0) {
+                  lastReadOffset = message.offset;
+                  callback(null);
+                }
+              });
+            },
+
+            function (callback) {
+              setImmediate(function () {
+                consumerGroupStream.close(callback);
+                consumerGroupStream = null;
+              });
+            },
+
+            function (callback) {
+              setTimeout(callback, 100);
+            },
+
+            function (callback) {
+              sendMessage([uuid.v4()], topic, callback);
+            },
+
+            function (callback) {
+              consumerGroupStream = createConsumerGroupStream(topic, { autoCommit: true, groupId: groupId });
+              consumerGroupStream.on('readable', function () {
+                const message = consumerGroupStream.read();
+                message.offset.should.be.equal(lastReadOffset + 1);
+                consumerGroupStream.close(callback);
+              });
+            }
+          ],
+          done
+        );
+      });
+    });
+
+    describe('#close', function () {
+      it('should not call consumerGroup with force option', function (done) {
+        const consumerGroupStream = createConsumerGroupStream(topic);
+
+        const closeSpy = sinon.spy(consumerGroupStream.consumerGroup, 'close');
+
+        consumerGroupStream.close(function () {
+          sinon.assert.calledOnce(closeSpy);
+          sinon.assert.calledWithExactly(closeSpy, false, sinon.match.func);
+          done();
+        });
+      });
+
+      it('autoCommit false should close the consumer without committing offsets', function (done) {
+        const messages = _.times(3, uuid.v4);
+        let consumerGroupStream;
+
+        async.series(
+          [
+            function (callback) {
+              sendMessage(messages, topic, callback);
+            },
+            function (callback) {
+              const messagesToRead = _.clone(messages);
+              consumerGroupStream = createConsumerGroupStream(topic, { groupId: groupId });
+              consumerGroupStream.on('data', function (message) {
+                _.pull(messagesToRead, message.value);
+                if (messagesToRead.length === 0) {
+                  callback(null);
+                }
+              });
+            },
+            function (callback) {
+              consumerGroupStream.close(callback);
+            },
+            function (callback) {
+              const messagesToRead = _.clone(messages);
+              consumerGroupStream = createConsumerGroupStream(topic, { groupId: groupId });
+              consumerGroupStream.on('data', function (message) {
+                _.pull(messagesToRead, message.value);
+                if (messagesToRead.length === 0) {
+                  callback(null);
+                }
+              });
+            },
+            function (callback) {
+              consumerGroupStream.close(callback);
+            }
+          ],
+          done
+        );
+      });
     });
   });
 });
