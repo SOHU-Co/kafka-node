34d29dd89c3a30fc8aed5f685f0f8092ae728b93
Ahmed Lafta <Aalafta@gmail.com>
2018-12-12 11:30:16 -0600

    add support for DescribeConfigs protocol (#1081)
    * add support for DescribeConfigs protocol
    
    * remove comment
    
    * update failing tests
    
    * remove test filtering
    
    * ensure done() is called for skipped tests
    
    * remove test filtering once again
    
    * PR comments
    
    * export valid resource types
    
    * update README

--- README.md
+++ README.md
@@ -1149,6 +1149,87 @@ admin.createTopics(topics, (err, res) => {
 
 See [createTopics](#createtopicstopics-cb)
 
+### describeConfigs(payload, cb)
+
+Fetch the configuration for the specified resources. It requires Kafka 0.11+.
+
+* `payload`: **Array**, array of resources
+* `cb`: **Function**, the callback
+
+Example:
+
+```js
+const resource = {
+  resourceType: admin.RESOURCE_TYPES.topic,   // 'broker' or 'topic'
+  resourceName: 'my-topic-name',
+  configNames: []           // specific config names, or empty array to return all,
+}
+
+const payload = {
+  resources: [resource],
+  includeSynonyms: false   // requires kafka 2.0+
+};
+
+admin.describeConfigs(payload, (err, res) => {
+  console.log(JSON.stringify(res,null,1));
+})
+```
+
+Result:
+
+```json
+[
+ {
+  "configEntries": [
+   {
+    "synonyms": [],
+    "configName": "compression.type",
+    "configValue": "producer",
+    "readOnly": false,
+    "configSource": 5,
+    "isSensitive": false
+   },
+   {
+    "synonyms": [],
+    "configName": "message.format.version",
+    "configValue": "0.10.2-IV0",
+    "readOnly": false,
+    "configSource": 4,
+    "isSensitive": false
+   },
+   {
+    "synonyms": [],
+    "configName": "file.delete.delay.ms",
+    "configValue": "60000",
+    "readOnly": false,
+    "configSource": 5,
+    "isSensitive": false
+   },
+   {
+    "synonyms": [],
+    "configName": "leader.replication.throttled.replicas",
+    "configValue": "",
+    "readOnly": false,
+    "configSource": 5,
+    "isSensitive": false
+   },
+   {
+    "synonyms": [],
+    "configName": "max.message.bytes",
+    "configValue": "1000012",
+    "readOnly": false,
+    "configSource": 5,
+    "isSensitive": false
+   },
+    ...
+  ],
+  "resourceType": "2",
+  "resourceName": "my-topic-name"
+ }
+]
+
+```
+
 # Troubleshooting / FAQ
 
 ## HighLevelProducer with KeyedPartitioner errors on first send
@@ -1317,6 +1398,12 @@ KAFKA_VERSION=0.9 npm test
 KAFKA_VERSION=0.10 npm test
 
 KAFKA_VERSION=0.11 npm test
+
+KAFKA_VERSION=1.0 npm test
+
+KAFKA_VERSION=1.1 npm test
+
+KAFKA_VERSION=2.0 npm test
 ```
 
 *See Docker hub [tags](https://hub.docker.com/r/wurstmeister/kafka/tags/) entry for which version is considered `latest`.
--- test/test.admin.js
+++ test/test.admin.js
@@ -2,7 +2,9 @@
 
 const Admin = require('../lib/admin');
 const ConsumerGroup = require('../lib/consumerGroup');
+const KafkaClient = require('../lib/kafkaClient');
 const uuid = require('uuid');
+const should = require('should');
 
 describe('Admin', function () {
   describe('#listGroups', function () {
@@ -85,6 +87,128 @@ describe('Admin', function () {
         res[nonExistentGroup].should.have.property('members').with.lengthOf(0);
         res[nonExistentGroup].should.have.property('state', 'Dead');
         done(error);
+      });
+    });
+  });
+
+  describe('#describeConfigs', function () {
+    const createTopic = require('../docker/createTopic');
+    let admin, client;
+    const topicName = uuid.v4();
+
+    before(function (done) {
+      if (['0.8', '0.9', '0.10'].includes(process.env.KAFKA_VERSION)) {
+        this.skip();
+      }
+
+      createTopic(topicName, 1, 1).then(function () {
+        client = new KafkaClient({kafkaHost: 'localhost:9092'});
+        admin = new Admin(client);
+        admin.once('ready', done);
+      });
+    });
+
+    after(function (done) {
+      if (client) {
+        client.close(done);
+      } else {
+        done();
+      }
+    });
+
+    it('should describe a list of topic configs', function (done) {
+      const request = {
+        resourceType: admin.RESOURCE_TYPES.topic,
+        resourceName: topicName,
+        configNames: []
+      };
+      const payload = {
+        includeSynonyms: false,
+        resources: [request]
+      };
+      admin.describeConfigs(payload, function (error, res) {
+        res.should.be.instanceof(Array);
+        res.length.should.be.exactly(1);
+        const entries = res[0];
+        entries.should.have.property('resourceType').and.exactly('2');
+        entries.should.have.property('resourceName').and.exactly(topicName);
+        entries.should.have.property('configEntries');
+        entries.configEntries.length.should.be.greaterThan(1);
+        done(error);
+      });
+    });
+
+    it('should describe a list of broker configs for a specific broker id', function (done) {
+      const brokerName = '1001';
+      const request = {
+        resourceType: admin.RESOURCE_TYPES.broker,
+        resourceName: brokerName,
+        configNames: []
+      };
+      const payload = {
+        includeSynonyms: false,
+        resources: [request]
+      };
+      admin.describeConfigs(payload, function (error, res) {
+        res.should.be.instanceof(Array);
+        res.length.should.be.exactly(1);
+        const entries = res[0];
+        entries.should.have.property('resourceType').and.exactly('4');
+        entries.should.have.property('resourceName').and.exactly(brokerName);
+        entries.should.have.property('configEntries');
+        entries.configEntries.length.should.be.greaterThan(1);
+        done(error);
+      });
+    });
+
+    it('should return an error if the resource (topic) doesnt exist', function (done) {
+      const request = {
+        resourceType: admin.RESOURCE_TYPES.topic,
+        resourceName: '',
+        configNames: []
+      };
+      const payload = {
+        includeSynonyms: false,
+        resources: [request]
+      };
+      admin.describeConfigs(payload, function (error, res) {
+        error.should.have.property('message').and.containEql('InvalidTopic');
+        done();
+      });
+    });
+
+    it('should return an error if the resource (broker) doesnt exist', function (done) {
+      const brokerId = '9999';
+      const request = {
+        resourceType: admin.RESOURCE_TYPES.broker,
+        resourceName: brokerId,
+        configNames: []
+      };
+      const payload = {
+        includeSynonyms: false,
+        resources: [request]
+      };
+      admin.describeConfigs(payload, function (error, res) {
+        should.not.exist(res);
+        error.should.have.property('message').and.containEql('Unexpected broker id');
+        done();
+      });
+    });
+
+    it('should return error for invalid resource type', function (done) {
+      const request = {
+        resourceType: 25,
+        resourceName: topicName,
+        configNames: []
+      };
+      const payload = {
+        includeSynonyms: false,
+        resources: [request]
+      };
+      admin.describeConfigs(payload, function (error, res) {
+        should.not.exist(res);
+        error.should.have.property('message').and.equal(`Unexpected resource type 25 for resource ${topicName}`);
+        done();
       });
     });
   });
