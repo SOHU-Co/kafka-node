5d3434ae6aa5de0e1885fc19beb2fff791ff5169
Roland Rust <github@roland.familie-rust.eu>
2019-09-03 12:50:05 +0200

    added topicPartitionCheckInterval to ConsumerGroupOptions | fixed type of onRebalance callback

--- types/index.d.ts
+++ types/index.d.ts
@@ -256,7 +256,8 @@ export interface ConsumerGroupOptions {
   retryMinTimeout?: number;
   connectOnReady?: boolean;
   heartbeatInterval?: number;
-  onRebalance?: () => Promise<void>;
+  topicPartitionCheckInterval?: number;
+  onRebalance?: (isMember: boolean, callback: (err?: Error) => void) => void;
 }
 
 export interface ConsumerGroupStreamOptions extends ConsumerGroupOptions {
@@ -344,7 +345,7 @@ export interface ClusterMetadataResponse {
   };
 }
 
-export interface MetadataResponse extends Array<BrokerMetadataResponse|ClusterMetadataResponse> {
+export interface MetadataResponse extends Array<BrokerMetadataResponse | ClusterMetadataResponse> {
   0: BrokerMetadataResponse;
   1: ClusterMetadataResponse;
 }
